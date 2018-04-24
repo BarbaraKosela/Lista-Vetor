4925de0c95cf6c03aaf493106f3307627f83eda7
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-04-24 15:03:57 -0300

    Adição de repositório
    Execução de atividades referente ao meio "Vetor"

---
+++ ConsoleApplication1/ConsoleApplication1/Exercicio01.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Exercicio01
+    {
+        public Exercicio01()
+        {
+            int[] numero = new int[16];
+
+            Console.Write("Número 1: ");
+            numero[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 2: ");
+            numero[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 3: ");
+            numero[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 4: ");
+            numero[3] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 5: ");
+            numero[4] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 6: ");
+            numero[5] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 7: ");
+            numero[6] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 8: ");
+            numero[7] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 9: ");
+            numero[8] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 10: ");
+            numero[9] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 11: ");
+            numero[10] = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Número 12: ");
+            numero[11] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 13: ");
+            numero[12] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 14: ");
+            numero[13] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 15: ");
+            numero[14] = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Número 16: ");
+            numero[15] = Convert.ToInt32(Console.ReadLine());
+
+            double soma = numero[0] + numero[1] + numero[2] + numero[3] + numero[4] + numero[5] + numero[6] + numero[7] +
+                numero[8] + numero[9] + numero[10] + numero[11] + numero[12] + numero[13] + numero[14] + numero[15];
+            
+            Console.WriteLine("A soma destes números é de: " + soma);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Exercicio02.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Exercicio02
+    {
+        public Exercicio02()
+        {
+            string[] nomes = new string[10];
+
+            Console.WriteLine("Nome 1: ");
+            nomes[0] = Console.ReadLine();
+
+            Console.WriteLine("Nome 2: ");
+            nomes[1] = Console.ReadLine();
+
+            Console.WriteLine("Nome 3 : ");
+            nomes[2] = Console.ReadLine();
+
+            Console.WriteLine("Nome 4: ");
+            nomes[3] = Console.ReadLine();
+
+            Console.WriteLine("Nome 5: ");
+            nomes[4] = Console.ReadLine();
+
+            Console.WriteLine("Nome 6: ");
+            nomes[5] = Console.ReadLine();
+
+            Console.WriteLine("Nome 7: ");
+            nomes[6] = Console.ReadLine();
+
+            Console.WriteLine("Nome 8: ");
+            nomes[7] = Console.ReadLine();
+
+            Console.WriteLine("Nome 9: ");
+            nomes[8] = Console.ReadLine();
+
+            Console.WriteLine("Nome 10: ");
+            nomes[9] = Console.ReadLine();
+
+
+
+        }
+    }
+}
